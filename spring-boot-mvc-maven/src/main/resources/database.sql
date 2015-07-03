8cae53a574024473721f84bdf7692b048533988a
maxlpy <maxlipengyu@gmail.com>
2015-07-02 21:37:58 -0400

    add sql

--- spring-boot-mvc-maven/src/main/resources/database.sql
+++ spring-boot-mvc-maven/src/main/resources/database.sql
@@ -1,2 +1,3 @@
-INSERT INTO `test`.`sas_artifactory` (`id`, `name`, `version`, `license`, `redistribution`, `approval`) VALUES ('1', 'boot', '2.0', 'Apach3.0', 'No', 'No');
-INSERT INTO `test`.`sas_artifactory` (`id`, `name`, `version`, `license`, `redistribution`, `approval`) VALUES ('2', 'tomcat', '7.0', 'Apach2.0', 'Yes', 'Yes');
+create table artifactorytpbs (id int, name varchar(100), version varchar(100), license varchar(100));
+INSERT INTO `test`.`sas_artifactory` (`id`, `name`, `version`, `license`) VALUES ('1', 'boot', '2.0', 'Apach3.0');
+INSERT INTO `test`.`sas_artifactory` (`id`, `name`, `version`, `license`) VALUES ('2', 'tomcat', '7.0', 'Apach2.0');
