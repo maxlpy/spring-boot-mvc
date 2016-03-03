94fd201ad4bfe44f9ba6953b222f53b35b7a7725
ccxxxx <chenxin715@gmail.com>
2016-03-03 15:07:06 -0500

    create OpenUI5

---
+++ spring-boot-mvc-gradle-OpenUI5/src/main/resources/database.sql
@@ -0,0 +1,3 @@
+create table artifactorytpbs (id int, name varchar(100), version varchar(100), license varchar(100));
+INSERT INTO `test`.`sas_artifactory` (`id`, `name`, `version`, `license`) VALUES ('1', 'boot', '2.0', 'Apach3.0');
+INSERT INTO `test`.`sas_artifactory` (`id`, `name`, `version`, `license`) VALUES ('2', 'tomcat', '7.0', 'Apach2.0');
